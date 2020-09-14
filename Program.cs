453fa92a05ab759f3c88d789b72a405e3b397df4
KellyCristinaAires <k.woys@hotmail.com>
2020-09-14 14:42:17 -0300

    asciiart

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace atividadeasciiart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("  ||  ||");
+            Console.WriteLine(@"  \\()//");
+            Console.WriteLine(@" //(__)\\");
+            Console.WriteLine(" ||    ||");
+        }
+    }
+}
